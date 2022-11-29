6e00a585ced2400d9db9a336ef4520df70e0dd85
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2022-11-29 13:45:47 +0000

    finished friends controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Friend.destroy_all
+
+Friend.create(id: 1, first_name: "david", last_name: "janes", user_id: 1)
+Friend.create(id: 2, first_name: "Shawshank", last_name: "Framed", user_id: 2)
+
+puts "seeded"
