a5076debab2c14df85bfc4dbc8aa47159776916a
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2022-12-02 13:45:53 +0000

    changes to navbar, and friends form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_151307) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_095553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,12 +64,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_151307) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "interest"
-    t.string "photo"
     t.float "latitude"
     t.float "longitude"
     t.string "adress"
     t.string "address"
+    t.string "interest"
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
